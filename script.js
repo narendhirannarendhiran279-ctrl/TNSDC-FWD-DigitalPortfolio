f51c5c62cd32bc19e1dbca810ec711181f1a98f6
narendhirannarendhiran279-ctrl <narendhirannarendhiran279@gmail.com>
2025-09-18 16:19:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollToSection(sectionId) {
+
+  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  alert("Thank you for reaching out! I'll get back to you soon.");
+
+});
